34da73a3af2cfe1ca143650b2f553256b0282945
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2024-05-04 06:15:44 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,9 +2,5 @@
 
 // Define a function to open the Facebook URL
 function openFacebook() {
-  // Define the Facebook URL
-  const facebookUrl = 'https://www.facebook.com/youssef.abdelatif12?mibextid=LQQJ4d';
-
-  // Open the Facebook URL in a new tab
-  window.open(facebookUrl, '_blank');
+  window.open('https://www.facebook.com/youssef.abdelatif12?mibextid=LQQJ4d', '_blank');
 }
