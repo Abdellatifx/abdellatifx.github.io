7b67dd014f7fe724d2d2824253aa1d5074233422
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2024-05-06 14:44:00 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 function fetchImage() {
-  // Call the API endpoint
-  fetch('http://127.0.0.1:8080/media')
+  fetch('/fetch-image')
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -8,13 +7,10 @@ function fetchImage() {
       return response.blob(); // Convert response to a Blob object
     })
     .then(blob => {
-      console.log(blob); // Log the blob data
-      // Create an object URL from the Blob
       const objectURL = URL.createObjectURL(blob);
-
-      // Open a new window/tab and set its content to display the image
-      const newWindow = window.open('');
-      newWindow.document.write(`<img src="${objectURL}" alt="Fetched Image">`);
+      const image = document.createElement('img');
+      image.src = objectURL;
+      document.body.appendChild(image);
     })
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error);
