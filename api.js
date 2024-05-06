c639beaac32812f5cee909d3db3446084f5e0144
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2024-05-06 18:59:17 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,24 +1,21 @@
-function fetchImage() {
+async function fetchImage() {
   const proxyUrl = 'https://api.allorigins.win/get?url='; // AllOrigins proxy URL
   const targetUrl = 'https://saragift-fghlck2qaa-uc.a.run.app/media'; // Your target URL
 
   // Call the API endpoint using the AllOrigins proxy
-  fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(targetUrl)}`)
-    .then(response => {
+  await fetch(proxyUrl+targetUrl)
+    .then(async response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      return response.blob(); // Convert response to JSON
+      return await response.json(); // Convert response to JSON
     })
-    .then(data => {
-      const blob = data.contents; // Extract blob from JSON response
-      console.log(blob);
-      console.log(blob.type);
-
+    .then(async data => {
+      const blob = await fetch(data.contents).then(res => res.blob());
       const objectURL = URL.createObjectURL(blob);
       const image = document.createElement('img');
       image.src = objectURL;
-      document.body.appendChild(image);
+      await document.body.appendChild(image);
     })
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error);
