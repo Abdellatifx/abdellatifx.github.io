c8269ce29f75ee679cbbb7e1b3c8a60acbaee029
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2024-05-06 03:32:45 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function fetchImage() {
   // Call the API endpoint
-  fetch('/media')
+  fetch('http://127.0.0.1:8000/media')
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
