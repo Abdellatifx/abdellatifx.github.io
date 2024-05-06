4a423ac9bf4844f02a9dd74a73e6c07bcd6c0aaf
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2024-05-06 15:37:07 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,22 @@
 function fetchImage() {
   fetch('/fetch-image')
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.blob(); // Convert response to a Blob object
+      // Convert response to a Blob object
+      return response.blob();
     })
     .then(blob => {
+      // Create object URL from the Blob
       const objectURL = URL.createObjectURL(blob);
+      
+      // Create an image element and set its source to the object URL
       const image = document.createElement('img');
       image.src = objectURL;
+      
+      // Append the image to the document body
       document.body.appendChild(image);
     })
     .catch(error => {
+      // Handle any errors that occur during the fetch operation
       console.error('There was a problem with the fetch operation:', error);
     });
 }
